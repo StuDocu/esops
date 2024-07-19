9596474b80a5732206302d2a7ac273d4131ae1f9
mustafaeskin <mustafasamedeskin@gmail.com>
2024-07-19 10:51:30 +0200

    minor improvements

--- src/esops/cli.py
+++ src/esops/cli.py
@@ -4,10 +4,10 @@ import uuid
 
 import click
 import numpy as np
-from matplotlib import pyplot as plt
 
 from esops.opt.agents import SMQLearningAgent, NonAgent
 from esops.envs import ConvertedRewardsEnv
+from esops.utils import make_timeline_plot
 
 
 np.seterr(divide='ignore')
@@ -27,10 +27,8 @@ class ExperimentContext:
     def __init__(self, seed, name: str = None):
         self.name = name
         self.seed = seed
-        if name is None:
-            self.id: str = uuid.uuid4().hex[:16]
-        else:
-            self.id = name
+        self.id: str = uuid.uuid4().hex[:16]
+
         self.path = f"./experiments/seed={self.seed}/{self.name}/run={self.id}"
 
     def setup(self):
@@ -83,7 +81,7 @@ def experiment(context, seed, name):
 
 @experiment.command()
 @click.option("--num-items", type=int, default=20)
-@click.option("--time-span", type=int, default=100)
+@click.option("--time-span", type=int, default=15)
 @click.option("--history-span", type=int, default=3000)
 @click.option("--alpha", type=float, default=0.1)
 @click.option("--gamma", type=float, default=0.9)
@@ -150,89 +148,6 @@ def run_q_learning(context, num_items, time_span, history_span, alpha, gamma, ep
 
 
 
-
-# @experiment.command()
-# @click.option('--course-id', type=int, default=4527748)
-# @click.option('--seed', type=int, default=12)
-# @click.option("--num-choices", type=int, default=2)
-# @click.option("--alpha", type=float, default=0.1)
-# @click.option("--gamma", type=float, default=0.9)
-# @click.option("--epsilon", type=float, default=0.1)
-# @click.pass_obj
-# def run_q_learning_init(context, course_id, seed, num_choices, alpha, gamma, epsilon):
-#     """
-#     poetry run simulate --name 'rl-experiment-baseline' run --alpha 0.1 --num-items 8 --num-choices=2 --time-steps 50
-#     """
-#     click.echo(F"Experiment ID: {context.name}")
-#     env = RealViewsConvertedRewardsEnv(course_id=course_id, seed=seed)
-#     agent = SoftQLearningAgent(
-#         ts=env.ts,
-#         num_items=env.num_items,
-#         alpha=alpha,
-#         gamma=gamma,
-#         epsilon=epsilon,
-#     )
-#
-#     context.add_config(
-#         {
-#             'agent': agent.name,
-#             'num_items': env.num_items,
-#             'num_choices': num_choices,
-#             'time_steps': env.ts,
-#             'alpha': alpha,
-#             'gamma': gamma,
-#             'epsilon': epsilon,
-#         }
-#     )
-#
-#     for t in range(1, env.ts):
-#         agent.step(env.R[:, t], num_choices)
-#     context.add_arrays(agent)
-#     context.add_sum_reward(env, agent)
-#
-#     plot = make_timeline_plot(env.M, agent.choices)
-#     context.add_plot(plot, 'view.plot.png')
-#
-#     plot = make_timeline_plot(env.R, agent.choices)
-#     context.add_plot(plot, 'rewards.plot.png')
-
-
-def make_timeline_plot(rewards, choices):
-    num_items, time_steps = rewards.shape
-
-    plt.figure(figsize=(32, 16))
-    colors = plt.cm.get_cmap('tab10', num_items)  # Use a colormap for different lines
-
-    for i in range(num_items):
-        plt.plot(
-            range(time_steps),
-            rewards[i, :],
-            label=f'Rewards Item {i}',
-            marker='o',
-            linestyle='-',
-            markersize=5,
-            color=colors(i)
-        )
-
-    for t, choice in enumerate(choices):
-        for c in choice:
-            plt.scatter(
-                t,
-                rewards[c, t],
-                color='red',
-                s=100,
-                edgecolors='black',
-                zorder=5,
-                label=f'Selected Item {c}' if t == 0 else None,
-            )
-
-    plt.xlabel('Timesteps', fontsize=14)
-    plt.ylabel('Views', fontsize=14)
-    plt.title('Rewards and Selections over Time', fontsize=16)
-    plt.legend(loc='upper left', bbox_to_anchor=(1, 1), fontsize=12)
-    plt.grid(True, linestyle='--', alpha=0.7)
-    plt.tight_layout()
-    return plt
 
 
 if __name__ == "__main__":
--- src/esops/utils.py
+++ src/esops/utils.py
@@ -1,5 +1,6 @@
 import contextlib
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 @contextlib.contextmanager
@@ -10,3 +11,41 @@ def temporary_seed(seed):
         yield
     finally:
         np.random.set_state(state)
+
+
+def make_timeline_plot(rewards, choices):
+    num_items, time_steps = rewards.shape
+
+    plt.figure(figsize=(32, 16))
+    colors = plt.cm.get_cmap('tab10', num_items)  # Use a colormap for different lines
+
+    for i in range(num_items):
+        plt.plot(
+            range(time_steps),
+            rewards[i, :],
+            label=f'Rewards Item {i}',
+            marker='o',
+            linestyle='-',
+            markersize=5,
+            color=colors(i)
+        )
+
+    for t, choice in enumerate(choices):
+        for c in choice:
+            plt.scatter(
+                t,
+                rewards[c, t],
+                color='red',
+                s=100,
+                edgecolors='black',
+                zorder=5,
+                label=f'Selected Item {c}' if t == 0 else None,
+            )
+
+    plt.xlabel('Timesteps', fontsize=14)
+    plt.ylabel('Views', fontsize=14)
+    plt.title('Rewards and Selections over Time', fontsize=16)
+    plt.legend(loc='upper left', bbox_to_anchor=(1, 1), fontsize=12)
+    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    return plt
